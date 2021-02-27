9d600abeba9dbb2cc457670c9e8bbb8b4e9da5df
devalio <git@devalio.ru>
2021-02-27 20:04:39 +0300

    Dragonfly finally removed out from go-ast repository

--- common.go
+++ common.go
@@ -1,6 +1,7 @@
 package builders
 
 import (
+	"fmt"
 	"go/ast"
 	"go/token"
 	"strings"
@@ -51,6 +52,29 @@ var (
 	// context.Context
 	ContextType = SimpleSelector("context", "Context")
 )
+
+func makeImportSpec(lPos *token.Pos, imports map[string]string) []ast.Spec {
+	var impSpec = make([]ast.Spec, 0, len(imports))
+	for packageKey, packagePath := range imports {
+		packageAlias := ast.NewIdent(packageKey)
+		pathSgms := strings.Split(packagePath, "/")
+		if pathSgms[len(pathSgms)-1] == packageKey {
+			packageAlias = nil
+		} else {
+			packageAlias.NamePos = *lPos
+		}
+		impSpec = append(impSpec, &ast.ImportSpec{
+			Name: packageAlias,
+			Path: &ast.BasicLit{
+				ValuePos: *lPos,
+				Kind:     token.STRING,
+				Value:    fmt.Sprintf("\"%s\"", packagePath),
+			},
+		})
+		*lPos++
+	}
+	return impSpec
+}
 
 // import declaration with token.IMPORT
 func Import(lPos *token.Pos, imports map[string]string) ast.Decl {
